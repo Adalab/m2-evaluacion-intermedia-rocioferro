2b6577b624346f0c33969b3ee956669bb97922f8
rocioferro <fer.rodriguezrocio@gmail.com>
2019-07-29 09:47:17 +0200

    end functions, start styles

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 'use strict';
 
 // Elementos
-const counter = document.querySelector('.counter');
+let counter = document.querySelector('.counter');
 const input = document.querySelector('.input'); 
 const btn = document.querySelector('.btn');
 const clue = document.querySelector('.paragraph');
 
 
 
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+
+const randomNum = getRandomNumber(100);
 
 // Función
 function readNumber (){
-    const value = input.value;
-    return console.log(value); 
+    const yourNumber = input.value;
+    console.log(yourNumber); 
+    counter.innerHTML= parseInt(counter.innerHTML) + 1
+    console.log(randomNum);
+    if (yourNumber<randomNum) {
+        clue.innerHTML='demasiado bajo';
+    }
+    else if (yourNumber>randomNum) {
+        clue.innerHTML='demasiado alto';
+    }
+    else {
+        clue.innerHTML='¡HAS GANADO, CAMPEONA!'
+    }
+
 }
-
-
-
-
-
-
 
 
 // Listener
